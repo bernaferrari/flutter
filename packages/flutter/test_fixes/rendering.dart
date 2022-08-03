5103642d657b4eab825173fa88f85c965034664d
BirjuVachhani <brvachhani@gmail.com>
2022-08-03 19:11:50 +0530

    Include rendering.dart for BoxDecoration flutter fix.

--- packages/flutter/test_fixes/rendering.dart
+++ packages/flutter/test_fixes/rendering.dart
@@ -5,6 +5,8 @@
 import 'package:flutter/rendering.dart';
 
 void main() {
+  BoxDecoration decoration;
+
   // Changes made in https://github.com/flutter/flutter/pull/66305
   RenderStack renderStack = RenderStack();
   renderStack = RenderStack(overflow: Overflow.visible);
@@ -18,4 +20,38 @@ void main() {
   renderListWheelViewport = RenderListWheelViewport(clipToSize: false);
   renderListWheelViewport = RenderListWheelViewport(error: '');
   renderListWheelViewport.clipToSize;
+
+  // Changes made in https://github.com/flutter/flutter/pull/105291
+  const BoxDecoration(color: Color(0xFFFF0000), boxShadow: []);
+  const BoxDecoration(
+    shape: BoxShape.rectangle,
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+  );
+  const BoxDecoration(
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    backgroundBlendMode: BlendMode.color,
+  );
+  const BoxDecoration(
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    border: Border.all(
+      color: Color(0xFF000000),
+      width: 2,
+    ),
+  );
+  const BoxDecoration(
+    shape: BoxShape.circle,
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    borderRadius: BorderRadius.circular(10),
+    gradient: LinearGradient(
+      colors: const [Color(0xFFFFFFFF), Color(0xFF000000)],
+    ),
+  );
+  decoration.copyWith(
+    color: Color(0xFFFF0000),
+    shape: BoxShape.rectangle,
+  );
 }
