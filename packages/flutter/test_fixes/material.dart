2884b8fc1a909926988d94043347a39320c1a72a
BirjuVachhani <brvachhani@gmail.com>
2022-08-03 18:45:22 +0530

    Tests for flutter fix

--- packages/flutter/test_fixes/material.dart
+++ packages/flutter/test_fixes/material.dart
@@ -569,4 +569,34 @@ void main() {
     primary: Colors.blue,
     onSurface: Colors.grey,
   );
+
+  // Changes made in https://github.com/flutter/flutter/pull/105291
+  const BoxDecoration(color: Color(0xFFFF0000), boxShadow: []);
+  const BoxDecoration(
+    shape: BoxShape.rectangle,
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+  );
+  const BoxDecoration(
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    backgroundBlendMode: BlendMode.color,
+  );
+  const BoxDecoration(
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    border: Border.all(
+      color: Color(0xFF000000),
+      width: 2,
+    ),
+  );
+  const BoxDecoration(
+    shape: BoxShape.circle,
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    borderRadius: BorderRadius.circular(10),
+    gradient: LinearGradient(
+      colors: const [Color(0xFFFFFFFF), Color(0xFF000000)],
+    ),
+  );
 }
--- packages/flutter/test_fixes/widgets.dart
+++ packages/flutter/test_fixes/widgets.dart
@@ -184,4 +184,34 @@ void main() {
 
   // Change made in https://github.com/flutter/flutter/pull/100381
   TextSelectionOverlay.fadeDuration;
+
+  // Changes made in https://github.com/flutter/flutter/pull/105291
+  const BoxDecoration(color: Color(0xFFFF0000), boxShadow: []);
+  const BoxDecoration(
+    shape: BoxShape.rectangle,
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+  );
+  const BoxDecoration(
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    backgroundBlendMode: BlendMode.color,
+  );
+  const BoxDecoration(
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    border: Border.all(
+      color: Color(0xFF000000),
+      width: 2,
+    ),
+  );
+  const BoxDecoration(
+    shape: BoxShape.circle,
+    color: Color(0xFFFF0000),
+    boxShadow: [],
+    borderRadius: BorderRadius.circular(10),
+    gradient: LinearGradient(
+      colors: const [Color(0xFFFFFFFF), Color(0xFF000000)],
+    ),
+  );
 }
